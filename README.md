b87f7baf5556e7f5f35399d3db545ce86afad68e
Philippe-Antoine Lehoux <plehoux@gmail.com>
2017-04-20 15:41:21 -0400

    Create brenchmark

--- README.md
+++ README.md
@@ -1,28 +1,10 @@
-# React starter project
-
-A minimal project to get started with React:
-
-* webpack and webpack-dev-server
-* ES6 and JSX via Babel 
-* hot module reloading via [React Hot Loader](https://github.com/gaearon/react-hot-loader)
-
-Installation:
-
-```
-git clone git@github.com:rauschma/react-starter-project.git
-cd react-starter-project
-npm install
-```
-
-Thanks to `npm run`, everything is installed locally!
-
-Development:
+# Run
 
 * Build once: `npm run webpack`
 * Run dev server: `npm start`
     * Web page: http://localhost:8080/
-    * Keep the console open to see errors and warnings
+    * Open the console
+    * Run `window.benchmark()`
 
-You can check out hot reloading by making changes to the JavaScript source code. You’ll see the results of those changes in the browser right away.
 
-No support for testing, yet, I’m still in the process of figuring out what I want there.
+Built using https://github.com/rauschma/react-starter-project
--- js/main.js
+++ js/main.js
@@ -1,26 +1,75 @@
 import React from "react";
 import {render} from 'react-dom';
 
-class Main extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            clickCount: 0,
-        };
-    }
-    render() {
-        return <div>
-            <a href="" onClick={this.handleClick.bind(this)}>Clicks: {this.state.clickCount}</a>
-        </div>;
-    }
-    handleClick(event) {
-        event.preventDefault();
-        this.setState({
-            clickCount: this.state.clickCount + 1,
-        });
-    }
+const Dot = (props) =>
+  <span>.</span>;
+
+class DotComponent extends React.Component {
+  render() {
+    return <span>.</span>;
+  }
 }
 
-render((
-    <Main/>
-), document.getElementById('main'));
+class Main extends React.Component {
+  render() {
+    var dots = Array(500).fill(0).map(x => {
+      if(this.props.kind == 'stateless-functional-direct-call') {
+        return Dot();
+      } else if(this.props.kind == 'stateless-functional-mounted') {
+        return React.createElement(Dot, {}, {});
+      } else if(this.props.kind == 'stateful') {
+        return React.createElement(DotComponent, {}, {});
+      }
+    })
+    return React.createElement('div', {}, ...dots);
+  }
+}
+
+let prevBenchmarkTime, benchmarkCount, statefulTotalTime, statelessFunctionalMountedTotalTime, statelessFunctionalDirectCallTotalTime;
+benchmarkCount = 0;
+statefulTotalTime = 0;
+statelessFunctionalMountedTotalTime = 0;
+statelessFunctionalDirectCallTotalTime = 0;
+
+const run = () => {
+  ['stateful', 'stateless-functional-mounted', 'stateless-functional-direct-call'].forEach(kind => {
+    const prevTime = performance.now();
+
+    var items = [];
+    var i, len;
+    for (i = 0, len = 20; i < len; i++) {
+      items.push(i);
+    }
+    items.forEach(i => {
+      render((
+        <Main kind={kind}/>
+        ), document.getElementById(kind));
+    });
+
+    const time = Math.round(performance.now() - prevTime);
+
+    if(kind == 'stateless-functional-direct-call') {
+      statelessFunctionalDirectCallTotalTime = statelessFunctionalDirectCallTotalTime + time
+    } else if(kind == 'stateless-functional-mounted') {
+      statelessFunctionalMountedTotalTime = statelessFunctionalMountedTotalTime + time
+    } else if(kind == 'stateful') {
+      statefulTotalTime = statefulTotalTime + time
+    }
+
+    const perf = (Math.round((1-time/prevBenchmarkTime)*100) || '  ')
+    prevBenchmarkTime = time;
+  })
+  prevBenchmarkTime = undefined
+  benchmarkCount = benchmarkCount + 1
+  console.log('.')
+  return
+}
+
+window.benchmark = (count=10) => {
+  console.log(`Running %c${count} %ctimes ...`, 'font-weight: bold', 'font-weight: normal');
+  Array(count).fill(0).forEach(x => run())
+  console.log(`Stateful                         took ${statefulTotalTime}ms`);
+  console.log(`Stateless Functional Mounted     took ${statelessFunctionalMountedTotalTime}ms %c${Math.round((1-statelessFunctionalMountedTotalTime/statefulTotalTime)*100)}% %c`, 'color:green', 'color:black');
+  console.log(`Stateless Functional Direct Call took ${statelessFunctionalDirectCallTotalTime}ms %c${Math.round((1-statelessFunctionalDirectCallTotalTime/statefulTotalTime)*100)}% %c`, 'color:green', 'color:black');
+  console.log(`%c🎉`, 'font-size: 100px')
+}
